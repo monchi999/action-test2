f0787fc7a145b2b984c9d2e93bdb15b0bfd4cc0e
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-3.compute.internal>
2024-09-22 01:08:05 +0000

    abc

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return jsonify(message="Hello, World!")
+    return jsonify(message="Hello, World!")
 
-    if __name__ == "__main__":
-            app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(debug=True)
